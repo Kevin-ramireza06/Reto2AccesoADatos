8e7571c64083bfcb025e4ce6255b7ebf04a258ee
AdrianQH18 <152377366+AdrianQH18@users.noreply.github.com>
2025-10-03 18:13:39 +0200

    apartado 3
    apartado3

---
+++ src/Reto2/MainPokemonesXML.java
@@ -0,0 +1,25 @@
+package Pokemones;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainPokemonesXML {
+    public static void main(String[] args) {
+        // 1. Crear lista de Pokémon
+        List<Pokemon> lista = new ArrayList<>();
+        lista.add(new Pokemon("Pikachu", "Sparky", 12, "Impactrueno,Placaje", 25));
+        lista.add(new Pokemon("Bulbasaur", "Leafy", 10, "Látigo Cepa,Placaje", 1));
+
+        // 2. Guardar la lista en XML
+        PokemonEscrituraXML.escribirA_XML(lista, "pokemones.xml");
+        System.out.println("XML generado correctamente.");
+
+        // 3. Leer la lista desde el XML
+        List<Pokemon> listaLeida = PokemonLecturaXML.leerDesdeXML("pokemones.xml");
+        System.out.println("Pokémon leídos del XML:");
+
+        // 4. Imprimir los Pokémon leídos
+        for (Pokemon p : listaLeida) {
+            System.out.println(p);
+        }
+    }
+}
